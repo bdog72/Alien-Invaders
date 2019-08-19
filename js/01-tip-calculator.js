3dd8b02aad3043acbae3bee32452cb1cc81409c7
Brian Bycynski <high5bri@hotmail.com>
2019-08-19 15:36:21 -0400

    finished section 5

--- js/01-tip-calculator.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('button');
-const output = document.querySelector('.output');
-
-button.addEventListener('click', function() {
-  const cost = document.querySelector('input');
-  let tip = (cost.value * 0.15).toFixed(2);
-  const temp = `<h1>You should tip $${tip} on $${cost.value}</h1>`;
-  output.innerHTML = temp;
-});
--- js/02-welcome-message.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('button');
-const output = document.querySelector('.output');
-const myName = document.querySelector('input');
-button.addEventListener('click', showMessage);
-
-function showMessage() {
-  output.innerHTML = `<h1>Hello ${myName.value}</h1>`;
-}
